64a627fa1513ce780fa15657c03eff1a23b06f1a
arivukaran <arivukaran@gmail.com>
2022-10-02 21:35:31 -0400

    core java learning

--- src/Employee.java
+++ src/Employee.java
@@ -2,7 +2,7 @@ public class Employee {
 
     private int EmpNo;
     private String Name;
-    private Department department;
+    private static Department department;
 
     public void setName(String name) {
         Name = name;
@@ -21,27 +21,30 @@ public class Employee {
     }
 
     public void Display() {
-        System.out.println("EmpNo:" + getEmpNo() + "  89Emp Name:" + getName() + " Dept:" + department.getDeptNo() );
+        System.out.println("EmpNo:" + getEmpNo() + "  89Emp Name:" + getName() + " Dept:" + department.getDeptNo());
     }
     public static void main(String[] args) {
 
         Employee e1 = new Employee();
         e1.setEmpNo(100);
         e1.setName("Prabhakaran");
+        department = new Department(1001);
 
-        try {
+       /* try {
             e1.department.setDeptNo(100);
         }
         catch(NullPointerException e)
         {
             System.out.println("Null pointer exception caught");
-        }
+        }*/
+
         e1.Display();
 
         Employee e2 = new Employee();
         e2.setEmpNo(101);
         e2.setName("Kaavya");
-        e2.department.setDeptNo(102);
+
+        //e2.department.setDeptNo(102);
         e2.Display();
     }
 }
